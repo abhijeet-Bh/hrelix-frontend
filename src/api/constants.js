4eacda552d8f121537092a7247bbb7832ae77987
abhijeet-Bh <abhijeetbhardwaj53@gmail.com>
2025-07-10 01:01:37 +0530

    feat: release version v1.0.0

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,6 +1,6 @@
-const dev = true;
+const dev = false;
 
 const BASE_URL_DEV = "http://localhost:8080";
-const BASE_URL_PROD = "https://your-production-url.com";
+const BASE_URL_PROD = "https://api-hrelix.blufin.co.in/";
 
 export const BASE_URL = dev ? BASE_URL_DEV : BASE_URL_PROD;
