bc2cbe57142d34b42c10a37aaa2a47c9fac85bb0
abhijeet-Bh <abhijeetbhardwaj53@gmail.com>
2025-07-29 17:40:11 +0530

    feat: employee search by email or name feature added

--- src/components/employees/EmployeeContext.jsx
+++ src/components/employees/EmployeeContext.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
-import { searchEmployeeByEmail } from "../../api/employeeApi";
+import {
+  getAllEmployees,
+  searchEmployeeByEmail,
+  searchEmployeeByEmailOrName,
+} from "../../api/employeeApi";
 import { EmployeeContext } from "./employee-context";
 
 export function EmployeeProvider({ children }) {
   const [searchResult, setSearchResult] = useState(null);
+  const [employeeList, setEmployeeList] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -22,9 +27,48 @@ export function EmployeeProvider({ children }) {
     }
   };
 
+  // search employee by email or name
+  const searchEmployeeEmailOrname = async (key) => {
+    if (!key) return;
+    setEmployeeList(null);
+    try {
+      setLoading(true);
+      const data = await searchEmployeeByEmailOrName(key);
+      setEmployeeList(data);
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data.error.message || "Failed to load dashboard");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // get all employee
+  const fetchAllEmployees = async () => {
+    setEmployeeList(null);
+    try {
+      setLoading(true);
+      const data = await getAllEmployees();
+      setEmployeeList(data);
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data.error.message || "Failed to load dashboard");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <EmployeeContext.Provider
-      value={{ searchResult, loading, error, searchEmployee }}
+      value={{
+        searchResult,
+        employeeList,
+        loading,
+        error,
+        searchEmployee,
+        searchEmployeeEmailOrname,
+        fetchAllEmployees,
+      }}
     >
       {children}
     </EmployeeContext.Provider>
